b57cc2a31533818838328c7ab14b6c77a08c635a
Cika <cika.lolita@hotmail.com>
2021-01-06 13:32:53 +0100

    gh pages

---
+++ build/precache-manifest.dfd4af5c72cf2c5e24dbba5c974c9a4c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea89dc1180cc27a308d9b781887df17a",
+    "url": "/cv-website/index.html"
+  },
+  {
+    "revision": "c830abad31cb6f09027f",
+    "url": "/cv-website/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "268f56f38b6b6e913b3b",
+    "url": "/cv-website/static/css/main.f1dfb855.chunk.css"
+  },
+  {
+    "revision": "c830abad31cb6f09027f",
+    "url": "/cv-website/static/js/2.b46c766d.chunk.js"
+  },
+  {
+    "revision": "daedb6e9c1e75eb2181f2dde37df55f3",
+    "url": "/cv-website/static/js/2.b46c766d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "268f56f38b6b6e913b3b",
+    "url": "/cv-website/static/js/main.fe6b8d88.chunk.js"
+  },
+  {
+    "revision": "34b2757fe09d631793b3",
+    "url": "/cv-website/static/js/runtime-main.65e5aaf3.js"
+  },
+  {
+    "revision": "2ca1bbb42e6f4f0e3941356ec8e8dbb8",
+    "url": "/cv-website/static/media/person1.2ca1bbb4.jpg"
+  }
+]);
